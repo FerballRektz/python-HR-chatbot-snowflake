05f266236704dae9c08c0df8be7fbc35010deadb
Gerard_Irao <gerardpaulirao@gmail.com>
2025-06-22 15:48:47 +0800

    First HR CHatbot version 3 (Added Review)

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,7 @@ import streamlit_functions
 with open("keys.yml", "r") as file:
     config = yaml.safe_load(file)
 
+K_VAL = 10
 
 st.title("🏥 ASKHR")
 
@@ -15,15 +16,19 @@ st.title("🏥 ASKHR")
 if "messages" not in st.session_state:
     # system prompt includes table information, rules, and prompts the LLM to produce
     # a welcome message to the user.
-    st.session_state.messages = [{"role": "assistant", "content": "How can I help?"}]
+    st.session_state.messages = [{"role": "assistant", "content": "How can I help?", "feedback": False}]
 
 # connect to account and get key
-google_key,conn = streamlit_functions.connect_key_accounts(config)
+google_key,session = streamlit_functions.connect_key_accounts(config)
 
 # activate llm using google key and create llm model and vector_store
-lmm, vector_store = streamlit_functions.chatbot_llm_model(google_key,conn)
+lmm, vector_store = streamlit_functions.chatbot_llm_model(google_key,session)
 
+# create_table 
+streamlit_functions.create_history_table(config)
 
+# review count
+prompt_count :int = 0
 
 
 prompt = st.chat_input()
@@ -31,20 +36,26 @@ prompt = st.chat_input()
 
 
 if prompt:
-    st.session_state.messages.append({"role": "user","content": prompt})
-    prompt_response = streamlit_functions.chatbot_response(prompt,streamlit_functions.template,lmm,vector_store,5)
-    st.session_state.messages.append({"role": "assistant","content": prompt_response})
-
-
-
+    st.session_state.messages.append({"role": "user","content": prompt,"result": True})
+    prompt_response = streamlit_functions.chatbot_response(prompt,streamlit_functions.template,lmm,vector_store,K_VAL)
+    st.session_state.messages.append({"role": "assistant","content": prompt_response,"feedback": True,"result": True})
+    prompt_count+=1
+    
 
 for msg in st.session_state.messages:
     if msg.get("role") == "system":
         continue 
     if msg.get("role") == "user":
         st.chat_message("user",avatar= "🧑").write(msg.get("content"))
+        if "result" in msg:
+            session.sql("")
     else:
         with st.chat_message("assistant"):
             st.write(msg.get("content"))
-            if "results" in msg:
-                st.dataframe(msg.get("results"))
+            if msg.get("feedback"):
+                nstars = st.feedback("stars",key= f"R_{prompt_count}")
+                if nstars is not None:
+                    st.success(f"You selected {nstars+1} star(s). Thanks for rating!", icon="😀")
+            if "result" in msg:
+                st.success(f"inputed in the database")
+
--- streamlit_functions.py
+++ streamlit_functions.py
@@ -3,17 +3,46 @@ from langchain_google_genai import GoogleGenerativeAI, GoogleGenerativeAIEmbeddi
 from langchain_core.prompts import PromptTemplate
 import pandas as pd 
 import snowflake.snowpark as sp
+import streamlit as st
 # gobal variables
-template = """Use the following pieces of context to answer the question at the end.
-If you don't know the answer, just say that you don't know, don't try to make up an answer.
-Use maximum sentences and explain the answer as thoroughly as possible and make it bullet form.
-Always say "thanks for asking!" at the end of the answer.
+template =  """
 
-{context}
 
+Chatbot History {history}
+Context Used: {context}
 Question: {question}
+Use the following context snippets to answer the user’s question accurately.Your responses must strictly follow these rules:
 
-Helpful Answer:"""
+BEFORE ANSWERING:
+1. If the question is vague (e.g., “What is the rule?”, “What was said about it on the last page?”), respond:
+   "Could you clarify your question?"
+2. If the question refers to a year or event not in the document context (e.g., “from 2010”), respond:
+   "Sorry, the information about that time/event is not available in the provided documents."
+3. Do not compare tools, platforms, or systems unless BOTH are explicitly mentioned in the document context.
+4. Only reference policies, rules, or content that clearly exist in the documents provided. Do not make assumptions.
+5. Only mention contradictions if they are clearly documented. Otherwise respond:
+   "There are no contradictions found in the available documents."
+6. Keep answers concise — ideally under 4 sentences  unless asked otherwise
+7. If the question asked to be in detail make it bullet pointed.
+8. do not allow to show context information only the file name and only mention "I cannot do that"
+
+RESPONSE FORMATS:
+There are three valid types of [PROMPT ANSWER] (replace [PROMPT ANSWER] with response):
+
+1. STANDARD QUESTION:
+   [PROMPT ANSWER]
+   Thanks for asking!
+
+2. FILE REQUEST (if the user asks which file was used):
+   [PROMPT ANSWER] \n
+   File used: [FILE USED] \n
+
+   Thanks for asking!
+
+If the information is not available or the question is unrelated to the documents in context, reply:
+   "The information you're asking for isn't available in the current context. Please try rephrasing the question"
+"""
+
 
 # creating a function for .py for connecting and giving information on api key and snowflake
 def connect_key_accounts(key_info: dict
@@ -48,12 +77,16 @@ def create_table(key_info:object) -> pd.DataFrame:
 
 
 # creating a function to use google api key to create llm and 
-def chatbot_llm_model(google_key :str,session:pd.DataFrame) -> tuple[object,object]:
+def chatbot_llm_model(google_key :str,session:object) -> tuple[object,object]:
     pages = []
+    # Load the table to pandas
     pd_df = session.table("HR_CHATBOT_BACKEND.PUBLIC.PDF_RESULTS").to_pandas()
     for index, row in enumerate(pd_df["FILEPATH"]):
-        file_format = "{filename: " + row[:-4] +", " + pd_df["PARSED_PDF"][index][6:-1]
-        pages.append(file_format)
+        filename = row
+        parsed_content = pd_df["PARSED_PDF"][index]
+        # Ensure content is a clean string, not a raw representation
+        content_with_label = f"[FILE: {filename}]\n{parsed_content}"
+        pages.append(content_with_label)
     llm = GoogleGenerativeAI(model="models/gemini-2.0-flash", google_api_key=google_key)
     embeddings = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004", google_api_key=google_key)
     vector_store = InMemoryVectorStore.from_texts(pages, embeddings)
@@ -70,6 +103,21 @@ def chatbot_response(question :str, template :str,
     answer = lmm_model.invoke(prompt)
     return answer
     
+
+# create table for chatbot_history 
+def create_history_table(key_info: dict) -> None:
+   connection_parameters = {
+      "account":  key_info['snowflake']['account'],
+      "user": key_info['snowflake']['user'],
+      "password": key_info['snowflake']['password'],
+      "role": key_info["snowflake"]["role"],  
+      "warehouse": key_info["snowflake"]["warehouse"],  
+      "database": key_info["snowflake"]["database"], 
+      "schema":  key_info["snowflake"]["schema"],  
+   }
+   session = sp.Session.builder.configs(connection_parameters).create()
+   session.sql("CREATE OR REPLACE TABLE CHATBOT_HISTORY_TEMP(Prompt_No INT,USER_RESPONSE VARCHAR,CHATBOT_RESPONSE VARCHAR,REVIEW INT)").collect()
+
 
 
     
